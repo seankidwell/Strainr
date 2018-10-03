27144e0810264ebb04d836d26cc1014a0ae05e56
seankidwell <dev.seankidwell@gmail.com>
2018-10-03 13:20:15 -0600

    modal close works

--- src/components/StrainCard/StrainCard.js
+++ src/components/StrainCard/StrainCard.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
 import './StrainCard.css';
-import Modal from 'react-modal';
+import Modal from 'react-responsive-modal';
 import axios from 'axios';
-Modal.setAppElement('#root');
 
 export default class StrainCard extends Component {
   constructor() {
@@ -27,8 +26,8 @@ export default class StrainCard extends Component {
   }
   
   handleCloseModal() {
-    console.log('Close Hit')
-    this.setState({ showModal: false, desc: 'it worked' });
+    this.setState({ showModal: false });
+    console.log('close hit')
   }
 
   render() {
@@ -40,44 +39,46 @@ export default class StrainCard extends Component {
     let species = this.props.species.charAt(0).toUpperCase(0) + this.props.species.slice(1);
 
     return (
-      <div className='strainCard' onClick={this.handleOpenModal}>
-          {this.props.name}
-          <Modal
-           isOpen={this.state.showModal}
-           onRequestClose={this.handleCloseModal}
-           className="modal"
-           overlayClassName="overlay"
-           shouldCloseOnOverlayClick={true}
-          >
-            <div className='title'>
-              <div className='name'>{this.props.name}</div>
-              <div className='species'>{species}</div>
-            </div>
-            <p className='description'>{this.state.desc}</p>
-            <div className='allLists'>
-              <div className='lists'>
-                <div className='list'>
-                  Positive Effects:
-                  <ul>{positive.length>0?positive:<li>None</li>}</ul>
-                </div>
-                <div className='list'>
-                  Negative Effects:
-                  <ul>{negative.length>0?negative:<li>None</li>}</ul>
-                </div>
+      <div>
+        <div className='strainCard' onClick={this.handleOpenModal}>
+            {this.props.name}
+        </div>
+        <Modal
+          open={this.state.showModal}
+          onClose={this.handleCloseModal}
+          classNames={{overlay: 'overlay', modal: 'modal'}}
+        //  className="modal"
+        //  overlayClassName="overlay"
+        //  shouldCloseOnOverlayClick={true}
+        >
+          <div className='title'>
+            <div className='name'>{this.props.name}</div>
+            <div className='species'>{species}</div>
+          </div>
+          <p className='description'>{this.state.desc}</p>
+          <div className='allLists'>
+            <div className='lists'>
+              <div className='list'>
+                Positive Effects:
+                <ul>{positive.length>0?positive:<li>None</li>}</ul>
               </div>
-              <div className='lists'>
-                <div className='list'>
-                  Medical Use For:
-                  <ul>{medicinal.length>0?medicinal:<li>None</li>}</ul>
-                </div>
-                <div className='list'>
-                  Flavors:
-                  <ul>{flavors}</ul>
-                </div>
+              <div className='list'>
+                Medical Use For:
+                <ul>{medicinal.length>0?medicinal:<li>None</li>}</ul>
               </div>
             </div>
-            <button onClick={this.handleCloseModal}>Close</button>
-          </Modal>
+            <div className='lists'>
+              <div className='list'>
+                Negative Effects:
+                <ul>{negative.length>0?negative:<li>None</li>}</ul>
+              </div>
+              <div className='list'>
+                Flavors:
+                <ul>{flavors}</ul>
+              </div>
+            </div>
+          </div>
+        </Modal>
       </div>
     )
   }
