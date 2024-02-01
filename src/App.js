30a5fb8e76a548c929774bf1ef4ab783bccfb3fd
seankidwell <dev.seankidwell@gmail.com>
2024-02-01 10:01:58 -0600

    alpho

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Nav from './components/Nav/Nav';
 import './App.css';
 
 class App extends Component {
-  //test123 do re mi fa so la ti do abcdefghijk
+  //test123 do re mi fa so la ti do abcdefghijkl
   render() {
     return (
       <div className="App">
