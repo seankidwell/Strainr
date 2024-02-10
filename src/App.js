384832576edf121f4dad02f02a45d6118a47df94
seankidwell <dev.seankidwell@gmail.com>
2024-02-09 21:30:59 -0600

    adjusting outline

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Nav from './components/Nav/Nav';
 import './App.css';
 
 class App extends Component {
-  //test123 do re mi fa so la ti do abcdefghijklmnop
+  //test123 do re mi fa so la ti do abcdefghijklmnopq
   render() {
     return (
       <div className="App">
