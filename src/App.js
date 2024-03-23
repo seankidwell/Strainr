5ab8e4961968dca0a88a877cc7dcd5244e16da6d
seankidwell <dev.seankidwell@gmail.com>
2024-03-22 22:33:23 -0500

    back in the ussr

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Nav from './components/Nav/Nav';
 import './App.css';
 
 class App extends Component {
-  //test123 do re mi fa so la ti do abcdefghijklmnopqrstuvwxyzzyxwvuts
+  //test123 do re mi fa so la ti do abcdefghijklmnopqrstuvwxyzzyxwvutsr
   render() {
     return (
       <div className="App">
