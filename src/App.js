65927e5119aeadecb50e7e26d3763f32df936cd0
seankidwell <dev.seankidwell@gmail.com>
2024-04-15 10:04:49 -0500

    igloo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Nav from './components/Nav/Nav';
 import './App.css';
 
 class App extends Component {
-  //test123 do re mi fa so la ti do abcdefghijklmnopqrstuvwxyzzyxwvutsrqponmlkj
+  //test123 do re mi fa so la ti do abcdefghijklmnopqrstuvwxyzzyxwvutsrqponmlkji
   render() {
     return (
       <div className="App">
