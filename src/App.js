8df6f04653ae221a2697286925172275fca7289c
seankidwell <dev.seankidwell@gmail.com>
2024-05-07 20:43:09 -0500

    cateral

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Nav from './components/Nav/Nav';
 import './App.css';
 
 class App extends Component {
-  //test123 do re mi fa so la ti do abcdefghijklmnopqrstuvwxyzzyxwvutsrqponmlkjihgfedcbab
+  //test123 do re mi fa so la ti do abcdefghijklmnopqrstuvwxyzzyxwvutsrqponmlkjihgfedcbabc
   render() {
     return (
       <div className="App">
